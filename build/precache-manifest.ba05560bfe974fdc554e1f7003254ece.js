897daadd2f12048ec845db5a8861322f1fd3df5c
Karthik Prakash Sivakumar <karthiksiva1997@gmail.com>
2023-06-07 17:21:07 -0400

    build

---
+++ build/precache-manifest.ba05560bfe974fdc554e1f7003254ece.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367f43ab15ab916bd36945d0e40bff14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3efb0bffbb2205d42656",
+    "url": "/static/css/2.4a0f89f5.chunk.css"
+  },
+  {
+    "revision": "982c7287a11b1cfb813c",
+    "url": "/static/css/main.fc127bf2.chunk.css"
+  },
+  {
+    "revision": "3efb0bffbb2205d42656",
+    "url": "/static/js/2.84a4d62a.chunk.js"
+  },
+  {
+    "revision": "1531e427017725bdb0bfbdd105c19ded",
+    "url": "/static/js/2.84a4d62a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "982c7287a11b1cfb813c",
+    "url": "/static/js/main.d0cb8a54.chunk.js"
+  },
+  {
+    "revision": "6f0cf1c2b3a8780a590c",
+    "url": "/static/js/runtime-main.812100ed.js"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/static/media/Roboto-Regular.f36638c2.ttf"
+  }
+]);
